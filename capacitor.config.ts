c8fe8afc00a2d1762572ad86eb348c47f3192535
MihajaAnd <angelo.lolot@gmail.com>
2025-04-18 18:54:33 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'etudiant-front',
+  webDir: 'www'
+};
+
+export default config;
